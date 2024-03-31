700d67e695c05bde9c8dbddcd1ae31e13bcfb300
Naman Sharma <namansharma3007c@gmail.com>
2024-03-31 11:36:52 +0530

    changes in readme

--- src/components/HeroContainer.jsx
+++ src/components/HeroContainer.jsx
@@ -29,7 +29,7 @@ const HeroContainer = () => {
         setCommand(command=>"");
         break;
       case "bio":
-        setCommand("");
+        setCommand(command=>"");
         setComponentsToRender([
           ...componentsToRender,
           {
